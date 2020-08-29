d6404e9d3fde1927725aaf6f5355e74cd240fb3b
Abhi <dabhiabhishek5601@gmail.com>
2020-08-29 16:24:06 -0400

    Updates

---
+++ precache-manifest.f29c2cca47c4201d09511ad0e57cadce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58dd846feb41ab94045f98a49d1ff333",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1e3882e415e5c532e6df",
+    "url": "/monsters-rolodex/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f34eaf95c0abf776f60d",
+    "url": "/monsters-rolodex/static/css/main.67989acd.chunk.css"
+  },
+  {
+    "revision": "1e3882e415e5c532e6df",
+    "url": "/monsters-rolodex/static/js/2.fd7102ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.fd7102ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f34eaf95c0abf776f60d",
+    "url": "/monsters-rolodex/static/js/main.53d81f38.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
