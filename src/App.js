b109a26245e6c0c7798b2962ec96128a76d20fdc
Abhi <dabhiabhishek5601@gmail.com>
2020-08-29 15:52:32 -0400

    styles added for font and color.

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends Component{
 
     return(
       <div className="App">
-
+        <h1> Monsters Rolodex </h1>
         <SearchBox handleChange = { this.handleChange } placeholder = "Search Monsters" />
         <CardList monsters={ filteredMonsters }/>
 
